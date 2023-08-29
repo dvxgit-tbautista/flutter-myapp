09fa17a1ea37f6e855dec9da09a0f1ddf5b1a731
Trixie Diane Bautista <tbautista@diavox.net>
2023-08-29 14:35:17 +0800

    scroll view

--- lib/main.dart
+++ lib/main.dart
@@ -16,85 +16,101 @@ class MyApp extends StatelessWidget {
         home: Scaffold(
           appBar: AppBar(title: const Text("Flutter Basics")),
           body: Container(
-            color: Colors.lightBlue,
-            child: Column(
-              mainAxisSize: MainAxisSize.max,
-              crossAxisAlignment: CrossAxisAlignment.start,
-              mainAxisAlignment: MainAxisAlignment.spaceAround,
-              children: [
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.start,
-                  children: [
-                    const Expanded(
-                        flex: 2,
-                        child: Text(
-                          'Taylor Swift',
-                          maxLines: 1,
-                          overflow: TextOverflow.ellipsis,
-                        )),
-                    Container(
-                      height: 20,
-                      width: 20,
-                      color: Colors.white,
-                    ),
-                    Container(
-                      height: 20,
-                      width: 20,
-                      color: Colors.green,
-                    ),
-                    const SizedBox(
-                      width: 8,
-                    ),
-                  ],
-                ),
-                Container(
-                  color: Colors.green,
-                  child: const Padding(
-                    padding: EdgeInsets.all(20.0),
-                    child: Text('First Column child'),
+            color: Colors.white,
+            // DO NOT USE EXPANDED IN A SCROLL VIEW VERTICALLY
+            child: SingleChildScrollView(
+              // physics: const BouncingScrollPhysics(),
+              child: Column(
+                mainAxisSize: MainAxisSize.max,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                // mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: [
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    children: [
+                      const Expanded(
+                          flex: 2,
+                          child: Text(
+                            'Taylor Swift',
+                            maxLines: 1,
+                            overflow: TextOverflow.ellipsis,
+                          )),
+                      Container(
+                        height: 20,
+                        width: 20,
+                        color: Colors.pinkAccent,
+                      ),
+                      Container(
+                        height: 20,
+                        width: 20,
+                        color: Colors.greenAccent,
+                      ),
+                      const SizedBox(
+                        width: 8,
+                      ),
+                    ],
                   ),
-                ),
-                const SizedBox(
-                  height: 20,
-                ),
-                Container(
-                  height: 200,
-                  width: 200,
-                  decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(10),
-                      color: Colors.amberAccent),
-                  child: const Center(
-                    child: Text(
-                      'Hello World!',
-                      style: TextStyle(color: Colors.red, fontSize: 20),
+                  Container(
+                    color: Colors.green,
+                    child: const Padding(
+                      padding: EdgeInsets.all(20.0),
+                      child: Text('First Column child'),
                     ),
                   ),
-                ),
-                Stack(
-                  children: [
-                    SizedBox(
-                      width: 100,
-                      height: 100,
-                      child: Image.asset(
-                        'assets/taylor.jpg',
-                        fit: BoxFit.cover,
+                  const SizedBox(
+                    height: 20,
+                  ),
+                  Container(
+                    height: 200,
+                    width: 200,
+                    decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(10),
+                        color: Colors.amberAccent),
+                    child: const Center(
+                      child: Text(
+                        'Hello World!',
+                        style: TextStyle(color: Colors.red, fontSize: 20),
                       ),
                     ),
-                    const Positioned(top: 80, child: Text('Taylor Swift'))
-                  ],
-                ),
-                const SizedBox(
+                  ),
+                  const SizedBox(
+                    height: 20,
+                  ),
+                  Stack(
+                    children: [
+                      SizedBox(
+                        width: 100,
+                        height: 100,
+                        child: Image.asset(
+                          'assets/taylor.jpg',
+                          fit: BoxFit.cover,
+                        ),
+                      ),
+                      const Positioned(
+                          top: 80, left: 10, child: Text('Taylor Swift'))
+                    ],
+                  ),
+                  const SizedBox(
+                    height: 20,
+                  ),
+                  const SizedBox(
+                      width: 100,
+                      height: 100,
+                      child: CircleAvatar(
+                        backgroundImage: NetworkImage(
+                            'https://images.wsj.net/im-213677?width=1280&size=1.33333333'),
+                      )),
+                  const SizedBox(
+                    height: 20,
+                  ),
+                  Container(
+                    height: 400,
                     width: 100,
-                    height: 100,
-                    child: CircleAvatar(
-                      backgroundImage: NetworkImage(
-                          'https://images.wsj.net/im-213677?width=1280&size=1.33333333'),
-                    )),
-                const SizedBox(
-                  height: 40,
-                ),
-                const Text('Last Column Child')
-              ],
+                    color: Colors.green,
+                    child: const Text("placeholder"),
+                  ),
+                ],
+              ),
             ),
           ),
           floatingActionButton: FloatingActionButton(
